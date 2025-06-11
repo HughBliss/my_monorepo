328d96e3dd03fa045396ff5186311988e8240270
Alexey Korzhavin <hughwols@gmail.com>
2025-06-11 19:18:11 +0500

    feat: init db package. init roles crud for admin.

---
+++ services/my_auth_service/internal/dto/role.go
@@ -0,0 +1,86 @@
+package dto
+
+import (
+	"github.com/hughbliss/my_database/pkg/gen/dbauth"
+	"github.com/hughbliss/my_protobuf/go/pkg/gen/common/v1"
+	roleserv1 "github.com/hughbliss/my_protobuf/go/pkg/gen/roles/v1"
+)
+
+type DomainRoles struct {
+	DomainID   string
+	DomainName string
+	Roles      []*Role
+}
+
+func DomainRolesFromEnt(e *dbauth.Domain) *DomainRoles {
+	roles := make([]*Role, 0)
+	for i, role := range e.Edges.Roles {
+		roles[i] = RoleFromEnt(role)
+	}
+
+	return &DomainRoles{
+		DomainID:   e.ID.String(),
+		DomainName: e.Name,
+		Roles:      roles,
+	}
+}
+
+func (r DomainRoles) ToProto() *roleserv1.DomainRoles {
+	roles := make([]*roleserv1.Role, len(r.Roles))
+	for i, role := range r.Roles {
+		roles[i] = role.ToProto()
+	}
+	return &roleserv1.DomainRoles{
+		Domain: &common.Domain{
+			Id:   r.DomainID,
+			Name: r.DomainName,
+		},
+	}
+}
+
+type Role struct {
+	Name        string
+	Description string
+	Permissions []string
+	DomainId    string
+}
+
+func RoleFromEnt(e *dbauth.Role) *Role {
+	return &Role{
+		Name:        e.Name,
+		Description: e.Description,
+		Permissions: e.Permissions,
+		DomainId:    e.DomainID.String(),
+	}
+}
+
+func (r *Role) ToProto() *roleserv1.Role {
+	return &roleserv1.Role{
+		Name:        r.Name,
+		Description: r.Description,
+		Permissions: r.Permissions,
+		DomainId:    r.DomainId,
+	}
+}
+
+type DomainRolesList []*DomainRoles
+
+func DomainRolesListFromEnt(e []*dbauth.Domain) DomainRolesList {
+	if len(e) == 0 {
+		return nil
+	}
+	res := make(DomainRolesList, len(e))
+	for i, domain := range e {
+		res[i] = DomainRolesFromEnt(domain)
+	}
+	return res
+}
+
+func (l DomainRolesList) ToProto() []*roleserv1.DomainRoles {
+	roles := make([]*roleserv1.DomainRoles, len(l))
+	for i, role := range l {
+		roles[i] = role.ToProto()
+	}
+	return roles
+
+}
---
+++ services/my_auth_service/internal/usecase/roles.go
@@ -0,0 +1,32 @@
+package usecase
+
+import (
+	"context"
+	"github.com/hughbliss/my_auth_service/internal/dto"
+	"github.com/hughbliss/my_database/pkg/gen/dbauth"
+	"github.com/hughbliss/my_toolkit/fault"
+	"github.com/hughbliss/my_toolkit/reporter"
+)
+
+const (
+	RolesGettingDBErr fault.Code = "RolesGettingDBErr" // RolesGettingDBErr: "ошибка получение ролей из базы данных"
+)
+
+type RolesUsecase struct {
+	rep reporter.Reporter
+	db  *dbauth.Client
+}
+
+func (r RolesUsecase) GetRoles(ctx context.Context) (dto.DomainRolesList, error) {
+	ctx, log, end := r.rep.Start(ctx, "GetRoles")
+	defer end()
+
+	all, err := r.db.Domain.Query().WithRoles().All(ctx)
+	if err != nil {
+		log.Err(err).Stack().Msg("failed to query all roles")
+		return nil, RolesGettingDBErr.Err()
+	}
+
+	return dto.DomainRolesListFromEnt(all), nil
+
+}
